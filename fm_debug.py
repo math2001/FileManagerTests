d53ad635229f57957eed1bc7df5a6406f22607e4
math2001 <australie.p@gmail.com>
2016-11-27 10:58:07 +1100

    better log

--- fm_debug.py
+++ fm_debug.py
@@ -7,13 +7,12 @@ import os.path
 class FmTestSideBarFolderExistsCommand(sublime_plugin.ApplicationCommand):
 
     def run(self, paths=None):
-        # return sublime.message_dialog(str( len(max('hello', 'hisdfdfds', 'hi', key=lambda path: len(repr(path)))) ))
         if paths is None:
             return sublime.error_message('Call it from the side bar, please')
-
-        # sublime.message_dialog('Have a look in the console (view -> show console)')
-        print('File Manager Tests')
-        print('Path '.center(len(repr(max(paths, key=lambda path: len(repr(path)))))) + ' | Exists | Is dir')
-        print('-' * (len(repr(max(paths, key=lambda path: len(repr(path)))))), '| ------ | ------')
+        max_path_length = len(repr(max(paths, key=lambda path: len(repr(path)))))
+        sublime.active_window().run_command('show_panel', {'panel': 'console'})
+        print('|', '-' * max_path_length, '| ------ | ------ |')
+        print('|', 'Path '.center(max_path_length) + ' | Exists | Is dir |')
+        print('|', '-' * max_path_length, '| ------ | ------ |')
         for path in paths:
-            print('{0!r} | {1} | {2}'.format(path, str(os.path.exists(path)).center(6), os.path.isdir(path)))
+            print('| {0!r} | {1!r:^6} | {2!r:^6} |'.format(path, os.path.exists(path), os.path.isdir(path)))
